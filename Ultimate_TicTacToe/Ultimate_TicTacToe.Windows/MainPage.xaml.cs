2ecc54a3e8df6ff1e3a7a79b4a515e812f512e22
Suhas Bharadwaj <suhas.five@gmail.com>
2014-09-15 23:25:25 +0530

    First Wave of Commits
    Committing for the project for the first time. Going to delete the empty
    grids and add the copy of the filled grid in their stead. Going to
    revert just in case alignment gets screwed up.

---
+++ Ultimate_TicTacToe/Ultimate_TicTacToe.Windows/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Ultimate_TicTacToe
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void TextBlock_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            TextBlock_1x1.Text = "O";
+        }
+
+        private void TextBlock_1x1_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
